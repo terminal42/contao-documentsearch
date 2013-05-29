91d2dec79cf06d7a0dfba3ec3ce31fc50b1f5ac8
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-05-29 18:26:16 +0200

    Class path adjustments

--- config/config.php
+++ config/config.php
@@ -20,23 +20,23 @@
 /**
  * Hooks
  */
-$GLOBALS['TL_HOOKS']['getSearchablePages'][]    = array('DocumentSearch\\DocumentSearchHelper', 'getSearchableDocuments');
+$GLOBALS['TL_HOOKS']['getSearchablePages'][]    = array('DocumentSearch\DocumentSearchHelper', 'getSearchableDocuments');
 
 /**
  * Sources
  */
 $GLOBALS['TL_DOCUMENT_SEARCH']['sources'] = array (
-    'DocumentSearch\\Source\\ContentElement'
+    'DocumentSearch\Source\ContentElement'
 );
 
 /**
  * Extractors
  */
 $GLOBALS['TL_DOCUMENT_SEARCH']['extractors'] = array (
-    'DocumentSearch\\Extractor\\Doc',
-    'DocumentSearch\\Extractor\\Pdf',
-    'DocumentSearch\\Extractor\\Ppt',
-    'DocumentSearch\\Extractor\\Rtf',
-    'DocumentSearch\\Extractor\\Text',
-    'DocumentSearch\\Extractor\\Xls'
+    'DocumentSearch\Extractor\Doc',
+    'DocumentSearch\Extractor\Pdf',
+    'DocumentSearch\Extractor\Ppt',
+    'DocumentSearch\Extractor\Rtf',
+    'DocumentSearch\Extractor\Text',
+    'DocumentSearch\Extractor\Xls',
 );
