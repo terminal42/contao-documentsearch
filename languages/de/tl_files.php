5a14a3e224d23d0ade32a8c565e4bfc2e888510d
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-05-30 08:49:24 +0200

    Added missing translations

---
+++ languages/de/tl_files.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2013
+ * @author     Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_files']['documentsearch_keywords']  = array('Schlüsselwörter für die DocumentSearch-Suche', 'Geben Sie hier eine kommagetrennte Liste von Schlüsselwörtern ein.');
---
+++ languages/en/tl_files.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2013
+ * @author     Yanick Witschi <yanick.witschi@terminal42.ch>
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_files']['documentsearch_keywords']  = array('Keywords for DocumentSearch', 'Please enter a comma-separated list of keywords.');
